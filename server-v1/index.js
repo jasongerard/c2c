0c81906a84a81a79330dcfd7ee2160a942813000
Jason Gerard <jason.gerard@gmail.com>
2016-08-04 19:56:07 -0400

    adding server 1.0.0

---
+++ server-v1/index.js
@@ -0,0 +1,14 @@
+"use strict";
+const restify = require("restify");
+const checks = require("node-healthcheck");
+const uuid = require("uuid").v4();
+
+const server = restify.createServer();
+const message = uuid + " version " + process.env.npm_package_version;
+checks.registerEndpoints(server);
+
+server.get("/", (req, res, next) =>{
+  res.send(200, message);
+});
+
+server.listen(8080, ()=> console.log('%s listening at %s', server.name, server.url));
